09e8bfc2129d66dd65fc34658092db360507fde6
kojotak <tomas.beneda@gmail.com>
2021-03-13 23:17:43 +0100

    akord je record misto classy

--- src/main/java/cz/kojotak/mtf/Akord.java
+++ src/main/java/cz/kojotak/mtf/Akord.java
@@ -5,22 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Akord {
+public record Akord (Ton ton, IntervalyAkordu intervaly) {
 
-	private final Ton ton;
-	private final IntervalyAkordu intervaly;
-	
-	public Akord(Ton ton, IntervalyAkordu intervaly) {
-		super();
-		this.ton = ton;
-		this.intervaly = intervaly;
-	}
-
-	@Override
-	public String toString() {
-		return "Akord [ton=" + ton + ", intervaly=" + intervaly.name() + "]";
-	}
-	
 	public List<Ton> getTonyAkordu(){
 		return Stream.concat(
 					Stream.of(ton),
