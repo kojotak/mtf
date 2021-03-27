23e9c1a66f9f3765e540f9f081eedbccd9bed9e7
kojotak <tomas.beneda@gmail.com>
2021-03-27 20:23:01 +0100

    test cleanup

--- src/test/java/cz/kojotak/mtf/AkordyTest.java
+++ src/test/java/cz/kojotak/mtf/AkordyTest.java
@@ -6,29 +6,29 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-class AkordyTest {
+class AkordyTest extends TestovaciTony {
 
 	@Test
 	void prvniObratkaCdur() {
-		Akord cdur = new Akord(new Ton(NazevTonu.C), TypAkordu.KVINTAKORD_DUROVY);
+		Akord cdur = new Akord(c, TypAkordu.KVINTAKORD_DUROVY);
 		List<Ton> prvniObrat = cdur.obratit().getTonyAkordu();
-		assertEquals(new Ton(NazevTonu.E), prvniObrat.get(0));
-		assertEquals(new Ton(NazevTonu.G), prvniObrat.get(1));
-		assertEquals(new Ton(NazevTonu.C), prvniObrat.get(2));
+		assertEquals(e, prvniObrat.get(0));
+		assertEquals(g, prvniObrat.get(1));
+		assertEquals(c, prvniObrat.get(2));
 	}
 	
 	@Test
 	void druhaObratkaCdur() {
-		Akord cdur = new Akord(new Ton(NazevTonu.C), TypAkordu.KVINTAKORD_DUROVY);
+		Akord cdur = new Akord(c, TypAkordu.KVINTAKORD_DUROVY);
 		List<Ton> druhyObrat = cdur.obratit().obratit().getTonyAkordu();
-		assertEquals(new Ton(NazevTonu.G), druhyObrat.get(0));
-		assertEquals(new Ton(NazevTonu.C), druhyObrat.get(1));
-		assertEquals(new Ton(NazevTonu.E), druhyObrat.get(2));
+		assertEquals(g, druhyObrat.get(0));
+		assertEquals(c, druhyObrat.get(1));
+		assertEquals(e, druhyObrat.get(2));
 	}
 	
 	@Test
 	void tretiObratkaCdur() {
-		Akord cdur = new Akord(new Ton(NazevTonu.C), TypAkordu.KVINTAKORD_DUROVY);
+		Akord cdur = new Akord(c, TypAkordu.KVINTAKORD_DUROVY);
 		List<Ton> tonyCdur = cdur.getTonyAkordu();
 		List<Ton> tretiObrat = cdur.obratit().obratit().obratit().getTonyAkordu();
 		assertEquals(tonyCdur.get(0), tretiObrat.get(0));
