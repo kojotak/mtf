0f521b4c518b0dbbec2611c851c57f684ea325ee
kojotak <tomas.beneda@gmail.com>
2021-04-14 21:57:31 +0200

    fixes #15

--- src/main/java/cz/kojotak/mtf/stupnice/ChromatickaStupnice.java
+++ src/main/java/cz/kojotak/mtf/stupnice/ChromatickaStupnice.java
@@ -2,12 +2,9 @@ package cz.kojotak.mtf.stupnice;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import cz.kojotak.mtf.Interval;
-import cz.kojotak.mtf.NazevTonu;
 import cz.kojotak.mtf.Stupnice;
 import cz.kojotak.mtf.Ton;
 
@@ -15,12 +12,18 @@ public record ChromatickaStupnice(Ton tonika) implements Stupnice {
 
 	@Override
 	public List<Ton> getTonyStupnice() {
-		LinkedList<Ton> list = new LinkedList<Ton>();
-		list.add(tonika);
-		IntStream.range(1, 13).forEach( i->
-			list.add( list.getLast().pridejInterval(Interval.SEKUNDA_MALA)) 
-		);
-		return list;
+		return IntStream.range(1, 13).collect( 
+				 ()->{ 
+					 LinkedList<Ton> list = new LinkedList<>();
+					 list.add( getTonika() );
+					 return list;
+					 },
+				 (l,el)->{
+					 Ton dalsi = l.getLast().pridejInterval(Interval.SEKUNDA_MALA);
+					 l.add(dalsi);
+				 },
+				 LinkedList::addAll
+				); 
 	}
 	
 	@Override
